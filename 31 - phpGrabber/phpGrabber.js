05ac206a1df7be9bbd8dd4632791caeabdd8716a
phiratio <phiratio161@gmail.com>
2018-04-06 22:47:12 +0300

    phpGrabber onefunctiondoesitall TM completed

--- 31 - phpGrabber/phpGrabber.js
+++ 31 - phpGrabber/phpGrabber.js
@@ -5,7 +5,7 @@
  *       \ V  V /  __/ |_) | |  _  | (_) \__ \ |_  |  _| (_| | (_|  __/
  *        \_/\_/ \___|_.__/  |_| |_|\___/|___/\__| |_|  \__,_|\___\___|
  *
- *                       PHP Extension Grabber v1.3
+ *                       PHP Extension Grabber v1.4
  */
 
 var grabber = (function defineGrabber() {
@@ -38,10 +38,37 @@ var grabber = (function defineGrabber() {
         return chkdExtns;
     }
 
-    function addThisVersion(version) {
+    function _getVersionExtensionsFromObjectWithExts(singleObject) {
+        let activeExtentionsForSingleVersion = [];
+        singleObject.data.map( object => {
+            if(object.status === 1){
+                activeExtentionsForSingleVersion.push(object.title);
+            }
+        });
+        return activeExtentionsForSingleVersion;
+    }
+
+    function _sendPostToGetExtentionsForSpecificVer(ver) {
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest(),
+                request_data = {"version": ver, "action": "extlist"};
+
+            request.open("POST", uri); // uri defined in template
+            request.onreadystatechange = function () {
+                if (request.readyState === 4 && request.status === 200) {
+                    _logger.logInfo(`PhP extensions for ${ver} acquired`);
+                    return resolve(JSON.parse(request.responseText))
+                }
+            };
+            request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.send(encodeFormData(request_data));
+        });
+    }
+
+    function _addThisVersion(version,objectWithExtentions) {
         allPHPExtensions.push({
-            version: version,
-            extensions: _getVersionExtensions()
+            version: Number(version.split(".").join("")),
+            extensions: _getVersionExtensionsFromObjectWithExts(objectWithExtentions)
         });
         _logger.logInfo(`Saved Extensions for ${version}`);
     }
@@ -79,15 +106,16 @@ var grabber = (function defineGrabber() {
         }
 
         function help() {
-            var $_$ = ` __        __   _       _   _           _     _____              
+            {let $_$ = ` __        __   _       _   _           _     _____              
  \\ \\      / /__| |__   | | | | ___  ___| |_  |  ___|_ _  ___ ___ 
   \\ \\ /\\ / / _ \\ '_ \\  | |_| |/ _ \\/ __| __| | |_ / _\` |/ __/ _ \\
    \\ V  V /  __/ |_) | |  _  | (_) \\__ \\ |_  |  _| (_| | (_|  __/
     \\_/\\_/ \\___|_.__/  |_| |_|\\___/|___/\\__| |_|  \\__,_|\\___\\___|
                                                                  
-                    PHP Extension Grabber v1.3`;
+                    PHP Extension Grabber v1.4`;
 
             console.log($_$);
+            }
             console.log("%c \\-- Select PHP Version from the Top Left dropdown, then ", style + "color: #8daed6;");
             console.log("  |-- grabber.add54() - %c All Extensions for PHP 5.4 are added to a JSON", "color: green;");
             console.log("  |-- grabber.add55() --  grabber.add56() --  grabber.add70() --  grabber.add71() --  grabber.add72()");
@@ -133,20 +161,18 @@ var grabber = (function defineGrabber() {
             });
         }
 
-        function selectItemByValue(elmnt, value) {
-            for (let i = 0; i < elmnt.options.length; i++) {
-                if (elmnt.options[i].value === value) {
-                    elmnt.selectedIndex = i;
-                    break;
-                }
-            }
+        function processReturnedJson(arrayOfObjects) {
+            arrayOfObjects.map( (snglObject, index) => {
+                _addThisVersion(availablePhpVersions[index],snglObject);
+            });
+            getJSON();
         }
 
         return Object.freeze({
             findVersionAmongSaved: findVersionAmongSaved,
             findVersIndex: findVersIndex,
             findAvailablePhpVersionsOnThisCpanel: findAvailablePhpVersionsOnThisCpanel,
-            selectItemByValue: selectItemByValue
+            processReturnedJson: processReturnedJson
         })
     }());
 
@@ -278,6 +304,30 @@ var grabber = (function defineGrabber() {
         _rmVer(72);
     }
 
+    function addCustom(ver) {
+                if (!/^[\d]{2}$/.test(ver)) {
+                        _logger.logErr("Invalid Version, Enter a Number Like: 71, 72, 73, 74")
+                    } else if (!_verifyCurrVersionDOM(ver)) {
+                        _logger.logErr("Wrong Version Selected");
+                    }  else if (_finder.findVersionAmongSaved(ver)) {
+                        _logger.logErr("Version" + ver + " already saved");
+                    } else {
+                        allPHPExtensions.push({
+                                version: ver,
+                                extensions: _getVersionExtensions()
+                        });
+                        _logger.logInfo("Saved Extensions for " + ver);
+                    }
+            }
+
+    function rmCustom(ver) {
+                if (!/^[\d]{2}$/.test(ver)) {
+                        _logger.logErr("Invalid Version, Enter a Number Like: 71, 72, 73, 74")
+                    }
+
+                    _rmVer(ver);
+            }
+
     function clear() {
         if (!_verifyExtensionsNotEmpty()) {
             _logger.logErr("THERE ARE NO EXTENSIONS TO REMOVE");
@@ -296,30 +346,21 @@ var grabber = (function defineGrabber() {
     }
 
     function addAll() {
-        const changeEvent = new Event("change");
-        let versionSelectorDom = document.querySelector("#user-selector-select-version");
-        let selectField = document.getElementById("user-selector-select-version");
-
         _finder.findAvailablePhpVersionsOnThisCpanel();
         console.warn(`php vers: ${availablePhpVersions}`);
         if(!availablePhpVersions || availablePhpVersions.length === 0){
             _logger.logErr("Something went wrong, contact this script developers.");
         }
 
-        availablePhpVersions.map( singleVersion => {
-            _finder.selectItemByValue(versionSelectorDom,singleVersion);
-            ((async () => {
-                selectField.dispatchEvent(changeEvent);
-            })()).then( result => {
-                addThisVersion(singleVersion);
-            });
+        var fireRequestForEachVerExtns = availablePhpVersions.map( singleVersion =>{
+           return _sendPostToGetExtentionsForSpecificVer(singleVersion);
         });
 
-        // (async function grabAllExtentions(phpVersions){
-        //     for(const singlePhpVersion of phpVersions){
-        //
-        //     }
-        // }(availablePhpVersions));
+        Promise.all(fireRequestForEachVerExtns).then( results =>{
+           _logger.logInfo(`All requests finished`);
+           _finder.processReturnedJson(results);
+        });
+
     }
 
     (function displayHelp() { _logger.help() }());
@@ -337,6 +378,8 @@ var grabber = (function defineGrabber() {
         rm71: rm71,
         add72: add72,
         rm72: rm72,
+        addCustom:addCustom,
+        rmCustom:rmCustom,
         v: reportStoredVersions,
         getJSON: getJSON,
         clear: clear,
@@ -347,52 +390,4 @@ var grabber = (function defineGrabber() {
 
 
 
-
-// Assuming we're listening for e.g. a 'change' event on `element`
-var event = new Event('change');
-var _bobec = document.getElementById("user-selector-select-version");
-
-function selectItemByValue(elmnt, value) {
-    for (let i = 0; i < elmnt.options.length; i++) {
-        if (elmnt.options[i].value === value) {
-            elmnt.selectedIndex = i;
-        }
-    }
-}
-_bobec.dispatchEvent(event);
-
-document.getElementById("user-selector-select-version").dispatchEvent(event);
-
-
-var _bobec2 = document.getElementsByTagName("option"); ///html collection
-var availablePhpVersions = [];
-
-Array.from(_bobec2).map( el => {
-    availablePhpVersions.push(String(el.value));
-    if(!availablePhpVersions){
-        _logger.logErr("Something went wrong, please contact this script developers!")
-    }
-});
-
-
-// Array.from(_bobec2).forEach(
-//     function(currentValue, currentIndex) {
-//         console.log(currentValue + ', ' + currentIndex);
-//     });
-async function asyncprocess() {
-    let number = Math.random()* 800;
-    setTimeout(()=> {
-        console.log(`Triggering event, the number is ${number}`);
-        console.log(Date.now());
-        return new Promise( (resolve, reject) => resolve("DONE!") );
-    }, number)
-}
-(async function parte(){
-    const num = 10;
-    for (let i = 0; i < num; i++){
-        //wait for the promise to resolve before advancing the for loop
-        await asyncprocess();
-        console.log(`Iteration ${i}`);
-    }
-}());
 
